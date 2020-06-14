c20d8cba43de250c37ea5874d6d8d080811bd9c4
liaoyl <liaoyl.lv@gmail.com>
2020-06-14 16:53:42 +0800

    initial commit

---
+++ base/src/main/java/com/yukon/BaseApplication.java
@@ -0,0 +1,12 @@
+package com.yukon;
+
+
+import org.springframework.boot.SpringApplication;
+
+public class BaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class, args);
+    }
+
+}
